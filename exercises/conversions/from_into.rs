dc5fbed21804734429d522949f2772f363559d9f
Rasmus Stougaard <rst@eucnordvest.dk>
2021-11-30 22:06:26 +0100

    Complete 'from into' exercise

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,28 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+        let parts: Vec<&str> = s.split(",").collect();
+        if parts.len() != 2 {
+            return Person::default();
+        }
+        let name = parts[0];
+        if name.is_empty() {
+            return Person::default();
+        }
+        let age = parts[1].parse::<usize>();
+
+        match age {
+            Ok(a) => Person {
+                name: String::from(name),
+                age: a,
+            },
+            Err(_) => Person::default()
+        }
     }
 }
 
